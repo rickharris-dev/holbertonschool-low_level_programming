ed50109a923afb960c0b654afda04ea19dd7d743
rickharris-dev <rickharris724@gmail.com>
2016-02-06 16:50:29 +0000

    Solved printing numbers without standard library.

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -2,7 +2,39 @@
 
 void print_number(int n)
 {
-  char a[];
-  a[]  = '0' + n;
-  char r = a[0];
+  int length, a;
+
+  if (n == 0)
+    {
+      print_char('0');
+    }
+
+  if (n < 0)
+    {
+      print_char('-');
+      n = n * -1;
+    }              
+
+  a = n;
+
+  for (length = 0; a >= 1; length++)
+    {
+      a = a / 10;
+    }
+
+  while (length > 0)
+    {
+      int c,d,i;
+      char b;
+      c = 1;
+      for (i = 1; length > i; i++)
+	{
+	  c = c * 10;
+	}
+      d = (n - (n % c)) / c;
+      n = n - (d * c);
+      b = '0' + d;
+      print_char(b);
+      length--;
+    }
 }
