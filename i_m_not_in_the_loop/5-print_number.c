d5c91949fe2d9261831498dfb9e52656376709bb
rickharris-dev <rickharris724@gmail.com>
2016-02-06 22:16:02 +0000

    Divided print number function into 3 functions.

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -1,40 +1,50 @@
 #include "my_functions.h"
 
+int calc_length(int n) 
+{
+  int l;
+
+  for (l = 0; n>=1; l++) 
+  {
+    n = n / 10;
+  }
+
+  return l;
+}
+
+void parse_print(int n, int l)
+{
+  while (l > 0)
+  {
+    int c,d,i;
+    char b;
+    c = 1;
+    for (i = 1; l > i; i++)
+      {
+        c = c * 10;
+      }
+    d = (n - (n % c)) / c;
+    n = n - (d * c);
+    b = '0' + d;
+    print_char(b);
+    l--;
+  }  
+}
+
 void print_number(int n)
 {
-  int length, a;
+  int l;
 
   if (n == 0)
     {
       print_char('0');
     }
-
-  if (n < 0)
+  else if (n < 0)
     {
       print_char('-');
       n = n * -1;
     }              
 
-  a = n;
-
-  for (length = 0; a >= 1; length++)
-    {
-      a = a / 10;
-    }
-
-  while (length > 0)
-    {
-      int c,d,i;
-      char b;
-      c = 1;
-      for (i = 1; length > i; i++)
-	{
-	  c = c * 10;
-	}
-      d = (n - (n % c)) / c;
-      n = n - (d * c);
-      b = '0' + d;
-      print_char(b);
-      length--;
-    }
+  l = calc_length(n);
+  parse_print(n,l);
 }
