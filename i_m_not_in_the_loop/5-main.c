de2afb176fb2a0eb7ba40b16080b2cd107dd2c29
rickharris-dev <rickharris724@gmail.com>
2016-02-08 23:50:28 +0000

    Modified code to account for minimum integer

--- i_m_not_in_the_loop/5-main.c
+++ i_m_not_in_the_loop/5-main.c
@@ -12,5 +12,11 @@ int main(void)
   print_char('\n');
   print_number(-98);
   print_char('\n');
+  print_number(-2147483647);
+  print_char('\n');
+  print_number(2147483647);
+  print_char('\n');
+  print_number(-2147483648);
+  print_char('\n');
   return (0);
 }
--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -1,54 +1,60 @@
 #include "my_functions.h"
 
-int calc_length(int n);              /* Function prototype */
-void parse_print(int n, int l);      /* Function prototype */
+int calc_length(int n);               /* Function prototype */
+void parse_print(int n, int l, int f);/* Function prototype */
 
-int calc_length(int n)               /* Function to get length of n */
+int calc_length(int n)                /* Function to get length of n */
 {
-  int l;                             /* Variable declaration */
+  int l;                              /* Variable declaration */
 
-  for (l = 0; n>=1; l++)             /* Determine length of number */ 
+  for (l = 0; n>=1; l++)              /* Determine length of number */ 
   {
-    n = n / 10;                      /* Divides n by 10 until < 0 */
+    n = n / 10;                       /* Divides n by 10 until < 0 */
   }
 
-  return l;                          /* Returns the length of the number */
+  return l;                           /* Returns the length of the number */
 }
 
-void parse_print(int n, int l)       /* Function to print each digit in n */
+void parse_print(int n, int l, int f) /* Function to print each digit in n */
 {
-  while (l > 0)                      /* Loop for the length of n */
+  while (l > 0)                       /* Loop for the length of n */
   {
-    int c,d,i;                       /* Variable declaration. */
-    char b;                          /* Variable declaration. */
+    int c,d,i;                        /* Variable declaration. */
+    char b;                           /* Variable declaration. */
 
-    for (i = 1, c = 1; l > i; i++)   /* Loop to create divisor */
+    for (i = 1, c = 1; l > i; i++)    /* Loop to create divisor */
       {
-        c = c * 10;                  /* Multiplies by 10 to create divisor */
+        c = c * 10;                   /* Multiplies by 10 to create divisor */
       }
-
-    d = (n - (n % c)) / c;           /* Sets d as first digit in n */
-    n = n - (d * c);                 /* Removes first digit in n */
-    b = '0' + d;                     /* Converts from int to char */
-    print_char(b);                   /* Sends number to be printed */
-    l--;                             /* Reduces length by one  */
+    if (l == 1)                       /* Determines if final digit */
+      {
+	n = n + f;                    /* Adds f =  0 if pos 1 if neg */
+      }
+    d = (n - (n % c)) / c;            /* Sets d as first digit in n */
+    n = n - (d * c);                  /* Removes first digit in n */
+    b = '0' + d;                      /* Converts from int to char */
+    print_char(b);                    /* Sends number to be printed */
+    l--;                              /* Reduces length by one  */
   }  
 }
 
-void print_number(int n)             /* Starts print process */
+void print_number(int n)              /* Starts print process */
 {
-  int l;                             /* Variable declaration. */
+  int l,f;                            /* Variable declaration. */
 
-  if (n == 0)                        /* Determines if the n is 0 */
+  f = 0;                              /* Sets f = 0 */
+
+  if (n == 0)                         /* Determines if the n is 0 */
     {
-      print_char('0');               /* Prints 0 */
+      print_char('0');                /* Prints 0 */
     }
-  else if (n < 0)                    /* Determines if n is negative */
+  else if (n < 0)                     /* Determines if n is negative */
     {
-      print_char('-');               /* Prints - symbol */
-      n = n * -1;                    /* Converts n to absolute value */
+      print_char('-');                /* Prints - symbol */
+      n = (n + 1) * -1;               /* Converts n to absolute value */
+      f = 1;                          /* Sets f as 1 if negative */
     }              
 
-  l = calc_length(n);                /* Stores length of number as l */
-  parse_print(n,l);                  /* Inititates the print function */
+  l = calc_length(n);                 /* Stores length of number as l */
+  parse_print(n,l,f);                 /* Inititates the print function */
 }
