c6a0ae03482a70e2a212a10113137204ed8aa0b6
rickharris-dev <rickharris724@gmail.com>
2016-02-12 21:05:57 +0000

    Function to print numbers using printf

---
+++ d/0-main.c
@@ -0,0 +1,16 @@
+#include "my_functions.h"
+
+int main(void)
+{
+  printf_number(98);
+  printf("\n");
+  printf_number(402);
+  printf("\n");
+  printf_number(1024);
+  printf("\n");
+  printf_number(0);
+  printf("\n");
+  printf_number(-98);
+  printf("\n");
+  return (0);
+}
---
+++ d/0-printf_number.c
@@ -0,0 +1,6 @@
+#include "my_functions.h"
+
+void printf_number(int n)
+{
+  printf("%d",n);
+}
