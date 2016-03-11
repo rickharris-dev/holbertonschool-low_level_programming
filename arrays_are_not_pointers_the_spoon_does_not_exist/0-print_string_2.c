814c7faa3895008698ff7bf538b63acbdea3b0de
rickharris-dev <rickharris724@gmail.com>
2016-03-11 20:24:12 +0000

    Script prints every other char starting with the first

---
+++ arrays_are_not_pointers_the_spoon_does_not_exist/0-print_string_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int print_char(char c);
+
+void print_string_2(char *str){
+  int i, j;
+  //Gets the length of the string
+  for (i = 0; *(str + i) != '\0'; i++){}
+  //Prints every other character starting with the first
+  for (j = 0; j < i; j+=2)
+    print_char(*(str + j));
+}
