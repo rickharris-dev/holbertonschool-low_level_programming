66e6a0c50d5bcad13b3da804ca07647768c5e220
rickharris-dev <rickharris724@gmail.com>
2016-03-16 23:57:27 +0000

    Script copies n characters of src to dest and copies 00 if no char to copy

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char *string_ncopy(char *dest, const char *src, int n)
+{
+  int i,j,s;
+
+  /* Finds the length of the dest */
+  for (i = 0; *(src + i) != '\0'; i++);
+
+  if (i + 1 < n)
+    s = i + 1;
+  else
+    s = n;
+
+  /* Store source string into dest including '\0' */
+  for (j = 0; j < s; j++)
+    *(dest + j) = *(src + j);
+
+  for (; j < n; j++)
+    *(dest + j) = 00;
+
+  /* Return address of the dest string */
+  return dest;
+}
