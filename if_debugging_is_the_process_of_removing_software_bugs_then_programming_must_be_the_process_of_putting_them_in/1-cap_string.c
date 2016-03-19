7de9a55f58f141d0e0e55e1b7733b4f2aa4f1959
rickharris-dev <rickharris724@gmail.com>
2016-03-18 21:40:51 -0700

    Script capitalizes first letter of string and letter after space, tab, and new line

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/1-cap_string.c
@@ -0,0 +1,9 @@
+char *cap_string(char *c)
+{
+  int i;
+  for (i = 0; c[i] != '\0'; i++)
+    if (c[i] > 96 && c[i] < 123)
+      if (i == 0 || c[i-1] == ' ' || c[i-1] == '\t' || c[i-1] == '\n')
+        c[i] -= 32;
+  return c;
+}
