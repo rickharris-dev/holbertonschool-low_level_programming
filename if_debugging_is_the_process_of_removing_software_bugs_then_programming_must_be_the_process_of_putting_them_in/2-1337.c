9f126acc41da6d48ff0b528824cc5bb60f099495
rickharris-dev <rickharris724@gmail.com>
2016-03-19 04:53:30 +0000

    Script converts specific letters to numbers

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/2-1337.c
@@ -0,0 +1,18 @@
+char *leet(char *c)
+{
+  int i;
+  for (i = 0; c[i] != '\0'; i++)
+  {
+    if ( c[i] == 'a' || c[i] == 'A')
+      c[i] = '4';
+    else if ( c[i] == 'e' || c[i] == 'E')
+      c[i] = '3';
+    else if ( c[i] == 'o' || c[i] == 'O')
+      c[i] = '0';
+    else if ( c[i] == 't' || c[i] == 'T')
+      c[i] = '7';
+    else if ( c[i] == 'l' || c[i] == 'L')
+      c[i] = '1';
+  }
+  return c;
+}
