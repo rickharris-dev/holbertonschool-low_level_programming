4ae19901aca1c0e9f55b7f896573fc82bedd8ad7
rickharris-dev <rickharris724@gmail.com>
2016-03-22 09:35:48 -0700

    Script rotates alpha characters by 13.

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/3-rot13.c
@@ -0,0 +1,12 @@
+char *rot13(char *c)
+{
+  int i;
+  for (i = 0; c[i] != '\0'; i++)
+  {
+    if((c[i] > 64 && c[i] < 78) || (c[i] > 96 && c[i] < 110))
+      c[i] += 13;
+    else if ((c[i] > 77 && c[i] < 91) || (c[i] > 109 && c[i] < 123))
+      c[i] -= 13;
+  }
+  return c;
+}
