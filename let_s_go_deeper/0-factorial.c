7116430951fbd9e0f86a6e32c5044b371cff56ee
rickharris-dev <rickharris724@gmail.com>
2016-03-28 19:02:24 +0000

    Optimized code

--- let_s_go_deeper/0-factorial.c
+++ let_s_go_deeper/0-factorial.c
@@ -10,13 +10,12 @@ int factorial(int n)
   else if (n == 0)
     return 1;
 
-  for (total = 1; n > 0; n--)
+  for (total = 1, test = 1; n > 0; n--)
   {
-    test = total;
     test *= n;
     if (test > INT_MAX)
       return -1;
-    total = total * n;
+    total = test;
   }
 
   return total;
