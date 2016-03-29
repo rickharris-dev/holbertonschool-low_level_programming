8264216f6da1367b7970134532b1b1f95c57f191
rickharris-dev <rickharris724@gmail.com>
2016-03-29 23:43:28 +0000

    Removed redundant code and optimized increments

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -1,25 +1,10 @@
-char* advance_s2_to_char(char *s2){
-  if (*s2 == 42)
-    return advance_s2_to_char(s2 + 1);
-  return s2;
-}
-
-char* advance_s1_to_match(char *s1, char *s2){
-  if (*s1 != *s2 && *s1 != '\0')
-    return advance_s1_to_match(s1 + 1, s2);
-  return s1;
-}
-
 int compare_strings(char *s1, char *s2, int c, int w){
-  if (*s2 == 42){
-    s2 = advance_s2_to_char(s2);
-    s1 = advance_s1_to_match(s1, s2);
-    return compare_strings(s1, s2, 0, 1);
-  }
+  if (*s2 == '*')
+    return compare_strings(s1, ++s2, 0, 1);
   else if (*s1 == *s2 && *s1 != '\0')
-    return compare_strings(s1 + 1, s2 + 1, c + 1, w);
+    return compare_strings(s1 + 1, ++s2, ++c, w);
   else if (*s1 != *s2 && w == 1)
-    return compare_strings(s1 - c + 1, s2 - c - 1, 0, 0);
+    return compare_strings(s1 - (c + 1), s2 - c, 0, 1);
   else if (*s1 == *s2)
     return 1;
   return 0;
