7c50c0151d75fa9a38249f84ebb0b1f2ae8907f9
rickharris-dev <rickharris724@gmail.com>
2016-03-29 22:35:13 +0000

    Built without using static

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -10,26 +10,21 @@ char* advance_s1_to_match(char *s1, char *s2){
   return s1;
 }
 
-int shell_comp(char *s1, char *s2)
-{
-  static int c = 0;
-  static int w = 0;
-  int r = 0;
+int compare_strings(char *s1, char *s2, int c, int w){
   if (*s2 == 42){
-    w = 1;
     s2 = advance_s2_to_char(s2);
     s1 = advance_s1_to_match(s1, s2);
-    c = 0;
-    return shell_comp(s1, s2);
-  } else if (*s1 == *s2 && *s1 != '\0') {
-    c++;
-    return shell_comp(s1 + 1, s2 + 1);
-  } else if (*s1 != *s2 && w == 1) {
-    w = 0;
-    return shell_comp(s1 - c + 1, s2 - c - 1);
-  } else if (*s1 == *s2)
-    r = 1;
-  c = 0;
-  w = 0;
-  return r;
+    return compare_strings(s1, s2, 0, 1);
+  }
+  else if (*s1 == *s2 && *s1 != '\0')
+    return compare_strings(s1 + 1, s2 + 1, c + 1, w);
+  else if (*s1 != *s2 && w == 1)
+    return compare_strings(s1 - c + 1, s2 - c - 1, 0, 0);
+  else if (*s1 == *s2)
+    return 1;
+  return 0;
+}
+
+int shell_comp(char *s1, char *s2) {
+  return compare_strings(s1, s2, 0, 0);
 }
