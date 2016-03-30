39be690c45f831aeebe89c714bd103259e4fc318
rickharris-dev <rickharris724@gmail.com>
2016-03-30 20:00:25 +0000

    Fix script to run with edge cases

--- let_s_go_deeper/5-main.c
+++ let_s_go_deeper/5-main.c
@@ -5,26 +5,108 @@ int shell_comp(char *s1, char *s2);
 int main(void)
 {
   int r;
-
+  r = shell_comp("a", "a");
+  if (r != 1)
+    printf("a, a\n");
+  r = shell_comp("aa", "a");
+  if (r != 0)
+    printf("aa, a\n");
+  r = shell_comp("aaa", "a");
+  if (r != 0)
+    printf("aaa, a\n");
+  r = shell_comp("a", "aa");
+  if (r != 0)
+    printf("a, aa\n");
+  r = shell_comp("ab", "ab");
+  if (r != 1)
+    printf("ab, ab\n");
+  r = shell_comp("abc", "*");
+  if (r != 1)
+    printf("abc, *\n");
+  r = shell_comp("abc", "****");
+  if (r != 1)
+    printf("abc, ****\n");
+  r = shell_comp("ab", "a*");
+  if (r != 1)
+    printf("ab, a*\n");
+  r = shell_comp("ab", "*a");
+  if (r != 0)
+    printf("ab, *a\n");
+  r = shell_comp("ab", "*c");
+  if (r != 0)
+    printf("ab, *c\n");
+  r = shell_comp("ab", "*b");
+  if (r != 1)
+    printf("ab, *b\n");
+  r = shell_comp("xyza", "*a");
+  if (r != 1)
+    printf("xyza, *a\n");
+  r = shell_comp("main.c", "*m*a*i*n*.*c*");
+  if (r != 1)
+    printf("main.c, *m*a*i*n*.*c*\n");
+  r = shell_comp("mainmain", "ma*");
+  if (r != 1)
+    printf("mainmain, ma*\n");
+  r = shell_comp("inn", "*n");
+  if (r != 1)
+    printf("inn, *n\n");
+  r = shell_comp("ma-main.c", "ma*in.c");
+  if (r != 1)
+    printf("ma-main.c, ma*in.c\n");
+  r = shell_comp("imain.c", "ma*in.c");
+  if (r != 0)
+    printf("imain.c, ma*in.c\n");
+  r = shell_comp("imain.c", "*in.c");
+  if (r != 1)
+    printf("imain.c, *in.c\n");
+  r = shell_comp("b", "a*");
+  if (r != 0)
+    printf("b, a*\n");
+  r = shell_comp("n.c", "*in.c");
+  if (r != 0)
+    printf("n.c, *in.c\n");
+  r = shell_comp("b", "");
+  if (r != 0)
+    printf("b, ''\n");
+  r = shell_comp("bb", "*ab");
+  if (r != 0)
+    printf("bb, *ab\n");
+  r = shell_comp("ab", "*a");
+  if (r != 0)
+    printf("ab, *a\n");
+  r = shell_comp("abb", "*ab");
+  if (r != 0)
+    printf("abb, *ab\n");
+  printf("---------------------------\n");
   r = shell_comp("main.c", "*.c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, *.c\n");
   r = shell_comp("main.c", "m*a*i*n*.*c*");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, m*a*i*n*.*c*\n");
   r = shell_comp("main.c", "main.c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, main.c\n");
   r = shell_comp("main.c", "m*c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, m*c\n");
   r = shell_comp("main.c", "ma********************************c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, ma********************************c\n");
   r = shell_comp("main.c", "*");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, *\n");
   r = shell_comp("main.c", "***");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, ***\n");
   r = shell_comp("main.c", "m.*c");
-  printf("%d\n", r);
+  if (r != 0)
+    printf("main.c, m.*c\n");
   r = shell_comp("main.c", "**.*c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main.c, **.*c\n");
   r = shell_comp("main-main.c", "ma*in.c");
-  printf("%d\n", r);
+  if (r != 1)
+    printf("main-main.c, ma*in.c\n");
   return (0);
 }
--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
+
 int compare_strings(char *s1, char *s2, int c, int w){
   if (*s2 == '*') /* Sets wildcard flag and advances s2 */
     return compare_strings(s1, ++s2, 0, 1);
+  else if (*(s2 - 1) == '*' && *s1 != *s2 && *s1 != '\0')
+    return compare_strings(++s1, s2, 0, w); /* Adv s1 to match after wildcard */
   else if (*s1 == *s2 && *s1 != '\0') /* Advances char comparison */
     return compare_strings(++s1, ++s2, ++c, w);
-  else if (*s1 != *s2 && w == 1)  /* If wildcard set, resets char after match */
-    return compare_strings(s1 - (c + 1), s2 - c, 0, 1);
+  else if (*s1 != *s2 && w == 1 && c > 0) /* If fail & wild set, resets char */
+    return compare_strings(s1 + (1 - c), s2 - c, 0, 1);
   else if (*s1 == *s2)  /* Returns 1 if strings match */
     return 1;
   return 0; /* Returns 0 if strings do not match */
