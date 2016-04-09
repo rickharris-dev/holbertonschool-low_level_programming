b5155306ed3858fc339823a8c6259f43e4d72157
rickharris-dev <rickharris724@gmail.com>
2016-04-09 01:11:54 +0000

    Added file comments and optimized operator check

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/get_op_func.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int op_add(int, int);
+int op_add(int, int); /* Function prototypes */
 int op_sub(int, int);
 int op_mul(int, int);
 int op_div(int, int);
@@ -7,10 +7,11 @@ int op_mod(int, int);
 
 int (*get_op_func(char c))(int a, int b){
   int i;
+  char op[6] = {'+', '-', '*', '/', '%'};
   int (*op_func[6])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
 
-  i = (c == '+')*1+(c == '-')*2+(c == '*')*3+(c == '/')*4+(c == '%')*5 - 1;
-  if (i == -1)
-    return NULL;
-  return (op_func[i]);
+  for (i = 0; i < 5; i++)
+    if (c == op[i]) /* Checks for a matching operator */
+      return op_func[i]; /* Return pointer to the function */
+  return NULL; /* If no match found, returns NULL */
 }
--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/2-calculator/main.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /*Required for printf() function */
+#include <stdlib.h> /* Required for atoi() function */
 
-int (*get_op_func(char c))(int, int);
+int (*get_op_func(char c))(int, int); /* Prototype declaration */
 
 int main(int c, char **v){
-  int (*r)(int,int);
+  int (*r)(int,int); /* Pointer to function variable declaration */
 
   if (c != 4)
-    return 1;
+    return 1; /* If not enough command line arguments, returns error */
 
   r = *get_op_func(*v[2]);
   if (*r == NULL)
-    return 1;
+    return 1; /* If function pointer is NULL, returns error */
 
-  printf("%d\n", (*r)(atoi(v[1]), atoi(v[3])));
-  return 0;
+  printf("%d\n", (*r)(atoi(v[1]), atoi(v[3]))); /* Prints returned integer */
+  return 0; /* Returns sucessful */
 }
