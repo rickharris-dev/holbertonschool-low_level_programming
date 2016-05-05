827ffbab4d3072471fb9aef9a074a175ef5df5eb
rickharris-dev <rickharris724@gmail.com>
2016-05-05 21:20:58 +0000

    Updated to store copy of string in new allocated memory

--- macros_and_structures/1-string_to_struct.c
+++ macros_and_structures/1-string_to_struct.c
@@ -11,10 +11,19 @@ int string_length(char *str) {
 struct String *string_to_struct(char *str) {
   /* Stores a string and its length in a newly allocated structure */
   struct String *string;
+  int i;
+  int l;
 
+  l = string_length(str);
   string = malloc(sizeof(struct String));
-  string->str = str;
-  string->length = string_length(str);
+  string->str = malloc(l + 1);
+  string->length = l;
+
+  for (i = 0; str[i] != '\0'; i++){
+    string->str[i] = str[i];
+  }
+
+  string->str[l] = '\n';
 
   return string;
 }
