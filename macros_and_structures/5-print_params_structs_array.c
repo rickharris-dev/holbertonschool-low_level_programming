f83114496a290d93ac5b2406d6eb50c6bd69b694
rickharris-dev <rickharris724@gmail.com>
2016-05-07 05:01:35 +0000

    Function prints the values stored in the array of structs

---
+++ macros_and_structures/5-print_params_structs_array.c
@@ -0,0 +1,83 @@
+#include "param_struct.h"
+#include <stdlib.h>
+#define ITEM(i) (sizeof(struct Param) * i)
+typedef struct Param param;
+
+int print_char(char c);
+
+int get_divisor (int l) {
+  /* Function creates a divisor to isolate each digit */
+  int d;
+
+  d = 1;
+  while (l - 1 > 0){
+    d = d * 10;
+    l--;
+  }
+  return d;
+}
+
+int number_length(int n){
+  /* Function finds the number of digits in a number */
+  int c;
+
+  c = 0;
+  if (n == 0)
+    return 1;
+  while(n >= 1) {
+    n = n / 10;
+    c++;
+  }
+  return c;
+}
+
+void print_number(int n){
+  /* Function prints a number one digit at a time */
+  int l;
+  int d;
+  char p;
+
+  l = number_length(n);
+  d = get_divisor(l);
+  while (l > 0){
+    p = '0' + (n / d);
+    print_char(p);
+    l--;
+    n = n % d;
+    d = d / 10;
+  }
+}
+
+void print_string(char *str){
+  /* Function prints a string */
+  int i;
+  for (i = 0; str[i] != '\0'; i++){
+    print_char(str[i]);
+  }
+}
+
+void print_array(char **array) {
+  /* Function prints an array of strings */
+  int i;
+
+  for (i = 0; array[i] != NULL; i++) {
+    if (i != 0){
+      print_char(',');
+      print_char(' ');
+    }
+    print_string(array[i]);
+  }
+}
+
+void print_params_structs_array(struct Param *array){
+  int i;
+
+  for (i = 0; array[i].str != NULL; i++) {
+    print_string(array[i].str);
+    print_char('\n');
+    print_number(array[i].length);
+    print_char('\n');
+    print_array(array[i].tab);
+    print_char('\n');
+  }
+}
